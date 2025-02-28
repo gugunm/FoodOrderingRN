6f03b1b0d6ac126b7de7c4886590441271797c91
Gugun M <mediamergugun2@gmail.com>
2025-02-28 14:35:11 +0700

    your first custom component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '../constants/Colors';
+
+export const ProductListItem = ({ product }: { product: any }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+});
